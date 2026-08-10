71c4e7bfb9ac378a9f088618715891939600e7fd
ogawast19083 <minionkawaii-kgunso@docomo.ne.jp>
2026-08-11 02:07:07 +0900

    動確開始しよう

--- firmware/esp32_serial_bridge/src/main.cpp
+++ firmware/esp32_serial_bridge/src/main.cpp
@@ -11,6 +11,7 @@ Copyright (c) 2025 RRST-NHK-Project. All rights reserved.
 ====================================================================*/
 
 #include "config.hpp"
+#include "v6.0_defs.hpp"
 #include "natsu_pid_task.hpp"
 #include "pin_ctrl_task.hpp"
 #include "robomas.hpp"
